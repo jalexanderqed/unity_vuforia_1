f200cac8ee002e465534ee5e05bb560982023543
John T. Alexander <jalexander.qed@gmail.com>
2016-11-20 21:29:07 -0800

    Implemented touch support.

--- Assets/GlobalScript.cs
+++ Assets/GlobalScript.cs
@@ -6,9 +6,10 @@ using Vuforia;
 public class GlobalScript : MonoBehaviour, ITrackableEventHandler {
 
     public Camera camera;
-    public Transform imageTarget;
+    public GameObject imageTarget;
     public TrackableBehaviour mTrackableBehaviour;
     public Transform[] teapots;
+    public GameObject ghostObject;
 
     private bool isTracked;
     private int pickedUp = -1;
@@ -16,7 +17,8 @@ public class GlobalScript : MonoBehaviour, ITrackableEventHandler {
     private int freeCount;
 
     private int freeThresh = 20;
-    private float distThresh = 0.8f;
+    private float distThresh = 0.5f;
+    private float epsilonDist = 0.1f;
 
     // Use this for initialization
     void Start()
@@ -33,33 +35,58 @@ public class GlobalScript : MonoBehaviour, ITrackableEventHandler {
         float minDistance = 1e10f;
         int minIndex = 0;
 
+        foreach(Touch touch in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Began)
+            {
+                RaycastHit hitObj;
+                Ray ray = camera.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray, out hitObj))
+                {
+                    if (pickedUp == -1)
+                    {
+                        for (int i = 0; i < teapots.Length; i++)
+                        {
+                            if (hitObj.transform.parent == teapots[i].transform)
+                            {
+                                pickedUp = i;
+                                pickUp(pickedUp, 0);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (hitObj.transform == imageTarget.transform)
+                        {
+                            putDown(pickedUp,
+                                teapots[pickedUp].transform.rotation.eulerAngles.y,
+                                hitObj.point.x,
+                                hitObj.point.z);
+                        }
+                    }
+                }
+            }
+        }
+
         for (int i = 0; i < teapots.Length; i++)
         {
-            float distance = Vector3.Distance(camera.transform.position, teapots[i].transform.position);
+            float distance = Vector3.Distance(ghostObject.transform.position, teapots[i].transform.position);
             if (distance < minDistance)
             {
                 minDistance = distance;
                 minIndex = i;
             }
         }
-        
-        Debug.Log("pickedUp: " + pickedUp);
 
         if (pickedUp == -1)
         {
             if (isFree)
             {
-                if (isTracked && minDistance < distThresh)
+                if (isTracked && minDistance < epsilonDist)
                 {
                     pickedUp = minIndex;
-                    isFree = false;
-                    freeCount = 0;
-
-                    teapots[pickedUp].transform.parent = camera.transform;
-                    teapots[pickedUp].transform.localPosition = new Vector3(0.0f, 0.0f, 0.8f);
-                    teapots[pickedUp].transform.localRotation = Quaternion.Euler(0, 90, -90);
-                    teapots[pickedUp].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-                    teapots[pickedUp].gameObject.GetComponentsInChildren<MeshRenderer>()[0].enabled = true;
+                    pickUp(pickedUp, teapots[pickedUp].transform.rotation.eulerAngles.y - ghostObject.transform.rotation.eulerAngles.y - 90);
                 }
             }
             else
@@ -77,15 +104,10 @@ public class GlobalScript : MonoBehaviour, ITrackableEventHandler {
             {
                 if (isTracked && camera.transform.position.y < distThresh)
                 {
-                    teapots[pickedUp].transform.parent = imageTarget.transform;
-                    teapots[pickedUp].transform.localPosition = new Vector3(0.4f, 0.0f, 0.0f);
-                    teapots[pickedUp].transform.localRotation = Quaternion.Euler(0, 0, 0);
-                    teapots[pickedUp].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-                    teapots[pickedUp].gameObject.GetComponentsInChildren<MeshRenderer>()[0].enabled = isTracked;
-
-                    pickedUp = -1;
-                    isFree = false;
-                    freeCount = 0;
+                    putDown(pickedUp, 
+                        teapots[pickedUp].transform.rotation.eulerAngles.y,
+                        teapots[pickedUp].transform.position.x,
+                        teapots[pickedUp].transform.position.z);
                 }
             }
             else
@@ -98,6 +120,38 @@ public class GlobalScript : MonoBehaviour, ITrackableEventHandler {
             }
         }
 	}
+
+    private void pickUp(int index, float yRot)
+    {
+        isFree = false;
+        freeCount = 0;
+
+        teapots[pickedUp].transform.parent = camera.transform;
+        teapots[pickedUp].transform.localPosition = new Vector3(0.0f, 0.0f, distThresh);
+        teapots[pickedUp].transform.localRotation =
+            Quaternion.Euler(
+                yRot,
+                90,
+                -90);
+        teapots[pickedUp].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        teapots[pickedUp].gameObject.GetComponentsInChildren<MeshRenderer>()[0].enabled = true;
+    }
+
+    private void putDown(int index, float yRot, float xPos, float zPos)
+    {
+        teapots[pickedUp].transform.parent = imageTarget.transform;
+        teapots[pickedUp].transform.localPosition = new Vector3(xPos, 0.0f, zPos);
+        teapots[pickedUp].transform.localRotation = Quaternion.Euler(
+            0,
+            yRot,
+            0);
+        teapots[pickedUp].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        teapots[pickedUp].gameObject.GetComponentsInChildren<MeshRenderer>()[0].enabled = isTracked;
+
+        pickedUp = -1;
+        isFree = false;
+        freeCount = 0;
+    }
 
     public void OnTrackableStateChanged(
                                 TrackableBehaviour.Status previousStatus,
---
+++ README.txt
@@ -0,0 +1,6 @@
+John Alexander
+8327934
+
+For this project, I elected to build an Android-based AR system with Vuforia and Unity. To make up for the increased ease of using Unity, I completed both of the extra-credit options. The app maintains the rotation of the teapot when it is picked up and put down, and the user can pick up and place the teapot by tapping on it.
+
+I had to delete the "Plugins" folder in order to make the file small enough, but all of my code should be present.
